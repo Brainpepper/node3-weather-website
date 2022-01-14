fa313d624c8c811bcc04c1fbbd873526987559e4
Lizardo Klavebäck <lizardoklaveback@Lizardos-MacBook-Air.local>
2022-01-14 21:11:17 +0100

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=effd1f220327775fb69ff2a76a99b027&query=' + latitude + ',' + longitude
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('unable to connect weather service!', undefined)
+        } else if (body.error) {
+            callback('unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out.' + ' Feels like ' + body.current.feelslike + ' degrees.')
+        }
+     })
+}
+
+module.exports = forecast
