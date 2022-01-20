ff2dcc4a21b10df099a8facfc1d30f1fd090e4cf
Lizardo Klavebäck <lizardoklaveback@Lizardos-MacBook-Air.local>
2022-01-20 20:07:43 +0100

    m

--- src/app.js
+++ src/app.js
@@ -1,104 +1,113 @@
-const express = require('express')
-const path = require('path')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars enginge and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Lizardo K'
-    })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Lizardo K",
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About me',
-        name: 'Lizardo K'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Lizardo K",
+  });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some helpfull text.',
-        title:'Help',
-        name: 'Lizardo K'
-    })
-})
+app.get("/:regnmr", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Lizardo K",
+    regnmr: req.params.regnmr,
+  });
+});
 
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpfull text.",
+    title: "Help",
+    name: "Lizardo K",
+  });
+});
 
-    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!",
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
-            return res.send({ error })
+          return res.send({ error });
         }
 
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
-    })
-})
-
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
     }
+  );
+});
 
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
 
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Lizardo K',
-        errorMessage: 'Help article not found.'
-    })
-})
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
 
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Lizardo K',
-        errorMessage: 'Page not found.'
-    })
-})
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Lizardo K",
+    errorMessage: "Help article not found.",
+  });
+});
 
-
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Lizardo K",
+    errorMessage: "Page not found.",
+  });
+});
 
 app.listen(port, () => {
-    console.log('Server is up on port port.')
-})
+  console.log("Server is up on port port.");
+});
